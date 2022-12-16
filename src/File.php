d547a1c0a39fb0dd316eaed9e3984a975c61e2f9
Lars Pinkenburg <lars.29palms@gmail.com>
2022-12-16 12:41:13 +0100

    speed improvements and add redis password to default config

--- src/File.php
+++ src/File.php
@@ -11,7 +11,7 @@ use TusPhp\Exception\OutOfRangeException;
 class File
 {
     /** @const Max chunk size */
-    public const CHUNK_SIZE = 8192; // 8 kilobytes.
+    public const CHUNK_SIZE = 8192 * 128; // 8 kilobytes * 128 * = 1M packets.
 
     /** @const Input stream */
     public const INPUT_STREAM = 'php://input';
@@ -426,7 +426,7 @@ class File
      */
     public function read($handle, int $chunkSize): string
     {
-        $data = fread($handle, $chunkSize);
+        $data = stream_get_contents($handle, $chunkSize);
 
         if (false === $data) {
             throw new FileException('Cannot read file.');
